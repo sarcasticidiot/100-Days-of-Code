4f577c249aa41d9c5fea20ac9912fdc8afd08273
Aksh Shaurya <shauryaaksh@gmail.com>
2026-08-14 10:34:25 +0530

    Create 1.c

---
+++ Day 2/1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{   
+    int l,b;
+    printf("Enter the sides of rectangle: (l and b) in m. \n");
+    scanf("%d",&l);
+    scanf("%d",&b);
+    printf("The perimeter of the rectangle is = %d m.\n",2*(l+b));
+    printf("The area of the rectangle is = %d m^2.\n",l*b);
+    return 0;
+}
